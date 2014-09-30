cffe800f99a695773e2a9d1a141417f4e9c24451
jlnewell <jason.newell@gmail.com>
2014-09-29 20:21:06 -0500

    107.1.0 release

--- src/SolidEdge.Community/Extensions/DocumentsExtensions.cs
+++ src/SolidEdge.Community/Extensions/DocumentsExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace SolidEdgeCommunity.Extensions
@@ -13,17 +14,49 @@ namespace SolidEdgeCommunity.Extensions
         /// <summary>
         /// Creates a new document.
         /// </summary>
-        internal static T Add<T>(this SolidEdgeFramework.Documents documents, string progId) where T : class
+        internal static TDocumentType Add<TDocumentType>(this SolidEdgeFramework.Documents documents, string progId) where TDocumentType : class
         {
-            return (T)documents.Add(progId);
+            return (TDocumentType)documents.Add(progId);
         }
 
         /// <summary>
         /// Creates a new document.
         /// </summary>
-        internal static T Add<T>(this SolidEdgeFramework.Documents documents, string progId, object TemplateDoc) where T : class
+        internal static TDocumentType Add<TDocumentType>(this SolidEdgeFramework.Documents documents, string progId, object TemplateDoc) where TDocumentType : class
         {
-            return (T)documents.Add(progId, TemplateDoc);
+            return (TDocumentType)documents.Add(progId, TemplateDoc);
+        }
+
+        /// <summary>
+        /// Creates a new document.
+        /// </summary>
+        /// <typeparam name="TDocumentType">SolidEdgeAssembly.AssemblyDocument, SolidEdgeDraft.DraftDocument, SolidEdgePart.PartDocument or SolidEdgePart.SheetMetalDocument.</typeparam>
+        /// <param name="documents"></param>
+        /// <returns></returns>
+        public static TDocumentType Add<TDocumentType>(this SolidEdgeFramework.Documents documents) where TDocumentType : class
+        {
+            var t = typeof(TDocumentType);
+
+            if (t.Equals(typeof(SolidEdgeAssembly.AssemblyDocument)))
+            {
+                return (TDocumentType)documents.AddAssemblyDocument();
+            }
+            else if (t.Equals(typeof(SolidEdgeDraft.DraftDocument)))
+            {
+                return (TDocumentType)documents.AddDraftDocument();
+            }
+            else if (t.Equals(typeof(SolidEdgePart.PartDocument)))
+            {
+                return (TDocumentType)documents.AddPartDocument();
+            }
+            else if (t.Equals(typeof(SolidEdgePart.SheetMetalDocument)))
+            {
+                return (TDocumentType)documents.AddSheetMetalDocument();
+            }
+            else
+            {
+                throw new NotSupportedException();
+            }
         }
 
         /// <summary>
@@ -88,6 +121,22 @@ namespace SolidEdgeCommunity.Extensions
         public static SolidEdgePart.SheetMetalDocument AddSheetMetalDocument(this SolidEdgeFramework.Documents documents, object TemplateDoc)
         {
             return documents.Add<SolidEdgePart.SheetMetalDocument>(SolidEdgeSDK.PROGID.SolidEdge_SheetMetalDocument, TemplateDoc);
+        }
+
+        /// <summary>
+        /// Opens an existing Solid Edge document.
+        /// </summary>
+        public static TDocumentType Open<TDocumentType>(this SolidEdgeFramework.Documents documents, string Filename) where TDocumentType : class
+        {
+            return (TDocumentType)documents.Open(Filename, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value);
+        }
+
+        /// <summary>
+        /// Opens an existing Solid Edge document.
+        /// </summary>
+        public static TDocumentType Open<TDocumentType>(this SolidEdgeFramework.Documents documents, string Filename, object DocRelationAutoServer, object AltPath, object RecognizeFeaturesIfPartTemplate, object RevisionRuleOption, object StopFileOpenIfRevisionRuleNotApplicable)
+        {
+            return (TDocumentType)documents.Open(Filename, DocRelationAutoServer, AltPath, RecognizeFeaturesIfPartTemplate, RevisionRuleOption, StopFileOpenIfRevisionRuleNotApplicable);
         }
     }
 }
--- test/QA/Program.cs
+++ test/QA/Program.cs
@@ -12,6 +12,14 @@ namespace QA
         [STAThread]
         static void Main(string[] args)
         {
+            var filename = @"C:\Program Files\Solid Edge ST7\Training\Coffee Pot.asm";
+            var application = SolidEdgeCommunity.SolidEdgeUtils.Connect(true, true);
+            var documents = application.Documents;
+
+            var assembly = documents.Add<SolidEdgeAssembly.AssemblyDocument>();
+            var draft = documents.Add<SolidEdgeDraft.DraftDocument>();
+            var part = documents.Add<SolidEdgePart.PartDocument>();
+            var sheetmetal = documents.Add<SolidEdgePart.SheetMetalDocument>();
         }
     }
 }
