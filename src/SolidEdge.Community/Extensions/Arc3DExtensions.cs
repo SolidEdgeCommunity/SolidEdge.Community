51e76cbb21f766a840e5c59f5cd5dde97999d58e
Jason Newell <jason.newell@outlook.com>
2015-09-09 02:36:49 -0500

    Added extensions methods.

---
+++ src/SolidEdge.Community/Extensions/Arc3DExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SolidEdgeCommunity.Extensions
+{
+    /// <summary>
+    /// SolidEdgePart.Arc3D extension methods.
+    /// </summary>
+    public static class Arc3DExtensions
+    {
+        public static double[] SafeGetKeypointPosition(this SolidEdgePart.Arc3D arc3d, SolidEdgePart.Sketch3DKeypointType KeypointType, bool throwOnError = false)
+        {
+            var position = Array.CreateInstance(typeof(double), 0);
+
+            try
+            {
+                // GetKeypointPosition may throw an exception so wrap in try\catch.
+                arc3d.GetKeypointPosition(KeypointType, ref position);
+            }
+            catch
+            {
+                if (throwOnError) throw;
+            }
+
+            return position.OfType<double>().ToArray();
+        }
+    }
+}
---
+++ src/SolidEdge.Community/Extensions/Line3DExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SolidEdgeCommunity.Extensions
+{
+    /// <summary>
+    /// SolidEdgePart.Line3D extension methods.
+    /// </summary>
+    public static class Line3DExtensions
+    {
+        public static double[] SafeGetKeypointPosition(this SolidEdgePart.Line3D line3d, SolidEdgePart.Sketch3DKeypointType KeypointType, bool throwOnError = false)
+        {
+            var position = Array.CreateInstance(typeof(double), 0);
+
+            try
+            {
+                // GetKeypointPosition may throw an exception so wrap in try\catch.
+                line3d.GetKeypointPosition(KeypointType, ref position);
+            }
+            catch
+            {
+                if (throwOnError) throw;
+            }
+
+            return position.OfType<double>().ToArray();
+        }
+    }
+}
