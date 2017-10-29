b4c6af635e77b76ead40cdc9c11c62ce0e89e8f5
Jason Newell <jason.newell@outlook.com>
2017-10-29 18:51:22 -0500

    ST10 & updated project to latest format

---
+++ src/QA/Program.cs
@@ -0,0 +1,20 @@
+﻿using SolidEdgeCommunity;
+using SolidEdgeCommunity.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var application = SolidEdgeUtils.Connect();
+            var documents = application.Documents;
+            var assemblyDocument = documents.AddAssemblyDocument();
+            var draftDocument = documents.AddDraftDocument();
+        }
+    }
+}
--- src/build_nuget.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set _nuget="..\packages\NuGet.CommandLine.2.8.5\tools\NuGet.exe"
-
-rem Create NuGet package and output to local NuGet path.
-for /r %%x in (*.nuspec) do %_nuget% pack "%%x" -o C:\NuGet\
-
-pause
